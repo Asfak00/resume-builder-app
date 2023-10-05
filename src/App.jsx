c08e237794940e0e9707fc8823b58044abf82c09
Asfak Ahmed <asfakahmed680@gmail.com>
2023-10-05 11:18:37 +0600

    working started now

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./index.scss";
+
+const App = () => {
+  return (
+    <div className="testing">
+      App
+      <div className="nesting">this is nesting</div>
+    </div>
+  );
+};
+
+export default App;
